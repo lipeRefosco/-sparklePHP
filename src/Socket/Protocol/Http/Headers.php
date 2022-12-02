319b7949017a9ed365811706cf7a72ecb46f6230
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-12-02 14:36:01 -0300

    feat[Headers]: adding class will handle each request and response headers

---
+++ src/Socket/Protocol/Http/Headers.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SparklePHP\Socket\Protocol\Http;
+
+class Headers {
+
+    public string $method;
+    public string $route;
+    public string $version;
+
+    function __construct(string $raw)
+    {
+        [$method, $route, $version, $headers] = self::parseHeaders($raw);
+        $this->method = $method;
+        $this->route = $route;
+        $this->version = $version;
+
+        foreach ($headers as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+    
+    static function parseHeaders(string $raw): array
+    {
+        $rawSplited = explode("\n", trim($raw));
+        $shiftIndice = fn(&$i) => array_shift($i);
+        
+        [$method, $route, $version] = self::extractMethodRoteVersion($rawSplited[0]);
+
+        $shiftIndice($rawSplited);
+
+        $headers = [];
+        foreach($rawSplited as $value) {
+            [$key, $val] = explode(": ", $value);
+            $key = $key;
+            $val = $val;
+        
+            $headers += [$key => $val];
+        }
+
+        return [$method, $route, $version, $headers];
+    }
+
+    static private function extractMethodRoteVersion(string &$firstRowHttpHeader): array
+    {
+        return explode(" ", $firstRowHttpHeader);
+    }
+    
+    public function set(string $key, string $value): void
+    {
+        $this->$key = $value;
+    }
+}
