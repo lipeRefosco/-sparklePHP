d66925316be56983af33e21662ea6b51a69f3650
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-12-08 22:48:43 -0300

    fix[Headers]: make the class more generic to use in request and response handlers

--- src/Socket/Protocol/Http/Headers.php
+++ src/Socket/Protocol/Http/Headers.php
@@ -4,52 +4,60 @@ namespace SparklePHP\Socket\Protocol\Http;
 
 class Headers {
 
+    public string $raw;
+    public array  $rawSplited;
     public string $method;
     public string $route;
     public string $version;
 
     function __construct(string $raw)
     {
-        [$method, $route, $version, $headers] = self::parseHeaders($raw);
-        $this->method = strtolower($method);
-        $this->route = strtolower($route);
-        $this->version = strtolower($version);
-
-        foreach ($headers as $key => $value) $this->set($key, $value);
-    }
-    
-    static function parseHeaders(string $raw): array
-    {
-        $removeFirstIndice = fn(&$i) => array_shift($i);
-
-        $rawSplited = array_map(
+        $this->raw = trim($raw);
+        $this->rawSplited = array_map(
             fn($string) => trim($string),
             explode("\n", trim($raw))
         );
-        [$method, $route, $version] = self::extractMethodRoteVersion($rawSplited[0]);
-
-        $removeFirstIndice( $rawSplited );
-
-        $headers = self::getHeadersFromRaw($rawSplited);
-
-        return [$method, $route, $version, $headers];
     }
 
-    static private function getHeadersFromRaw(array $rawSplited): array
+    public function parseRaw(): void
     {
-        $headers = [];
+        $this->setMethod();
+        $this->setRoute();
+        $this->setHttpVersion();
+        $this->setFieldsAndValues();
+    }
+
+    private function setMethod():void
+    {
+        [$method] = explode(" ", $this->rawSplited[0]);
+        
+        $method = strtoupper($method);
+
+        $this->method = $method;
+    }
+
+    private function setRoute(): void
+    {
+        [$_, $route] = explode(" ", $this->rawSplited[0]);
+
+        $this->route = $route;
+    }
+
+    private function setHttpVersion(): void
+    {
+        [$_, $_, $httpVersion] = explode(" ", $this->rawSplited[0]);
+
+        $this->version = $httpVersion;
+    }
+
+    private function setFieldsAndValues(): void
+    {
+        $rawSplited = array_slice($this->rawSplited, 1);
         
         foreach($rawSplited as $value) {
             [$key, $val] = explode(": ", $value);
-            $headers += [$key => $val];
+            $this->set($key, $val);
         }
-
-        return $headers;
-    }
-
-    static private function extractMethodRoteVersion(string $firstRowHttpHeader): array
-    {
-        return explode(" ", $firstRowHttpHeader);
     }
     
     public function set(string $key, string $value): void
--- tests/HeadersTest.php
+++ tests/HeadersTest.php
@@ -6,9 +6,9 @@ use SparklePHP\Socket\Protocol\Http\Headers;
 
 final class HeadersTest extends TestCase
 {
-    public function testCanExtractMethodRouteAndHttpVersionFromRequestHeader(): void
+    public function testHeaderClass():void
     {
-        $data = <<<END
+        $raw = <<<END
         GET /favicon.ico HTTP/1.1
         Host: localhost:8080
         User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0
@@ -17,33 +17,19 @@ final class HeadersTest extends TestCase
         Accept-Encoding: gzip, deflate, br
         END;
 
-        $expected = [
-                "GET",
-                "/favicon.ico",
-                "HTTP/1.1",
-                [
-                    "Host" => "localhost:8080",
-                    "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
-                    "Accept" => "image/avif,image/webp,*/*",
-                    "Accept-Language" => "en-US,en;q=0.5",
-                    "Accept-Encoding" => "gzip, deflate, br",
-                ]
-            ];
-
-        $result = Headers::parseHeaders($data);
-
-        $this->assertEquals(
-            $expected,
-            $result
-        );
-    }
-
-    public function testHeaderClass():void
-    {
         $expected = (Array)[
-            "method"          => "get",
+            "raw"             => $raw,
+            "rawSplited"      => [
+                "GET /favicon.ico HTTP/1.1",
+                "Host: localhost:8080",
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
+                "Accept: image/avif,image/webp,*/*",
+                "Accept-Language: en-US,en;q=0.5",
+                "Accept-Encoding: gzip, deflate, br"
+            ],
+            "method"          => "GET",
             "route"           => "/favicon.ico",
-            "version"         => "http/1.1",
+            "version"         => "HTTP/1.1",
             "host"            => "localhost:8080",
             "user-agent"      => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
             "accept"          => "image/avif,image/webp,*/*",
@@ -51,15 +37,9 @@ final class HeadersTest extends TestCase
             "accept-encoding" => "gzip, deflate, br"
         ];
 
-        $result = (Array)new Headers(<<<END
-        GET /favicon.ico HTTP/1.1
-        Host: localhost:8080
-        User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0
-        Accept: image/avif,image/webp,*/*
-        Accept-Language: en-US,en;q=0.5
-        Accept-Encoding: gzip, deflate, br
-        END);
+        $result = new Headers($raw);
+        $result->parseRaw();
 
-        $this->assertEquals($expected, $result);
+        $this->assertEquals($expected, (array)$result);
     }
 }
