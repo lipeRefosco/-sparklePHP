dd1a72a4812b7cdd3b78bca35123bb36c0dee374
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-12-24 18:05:27 -0300

    feat[Headers]: added method to be handled by Request and Response

--- src/Socket/Protocol/Http/Headers.php
+++ src/Socket/Protocol/Http/Headers.php
@@ -16,42 +16,29 @@ class Headers {
         $this->rawSplited = self::splitRaw($raw);
     }
 
+    /**
+     * Trim and explode the raw request input
+     * @param string
+     * @return array 
+    */
     static private function splitRaw(string $raw): array
     {
-        return array_map( fn($string) => trim($string),
-                          explode("\n", trim($raw))
-                        );
+        $rawSplited = explode("\n", trim($raw));
+
+        return array_map(
+            fn($string) => trim($string),
+            $rawSplited
+        );
     }
 
     public function parseRaw(): void
     {
-        $this->setMethod();
-        $this->setRoute();
-        $this->setHttpVersion();
+        [$method, $route, $httpVersion] = explode(" ", $this->rawSplited[0]);
+
+        $this->set("method", $method);
+        $this->set("route", $route);
+        $this->set("version", $httpVersion);
         $this->setFieldsAndValues();
-    }
-
-    private function setMethod():void
-    {
-        [$method] = explode(" ", $this->rawSplited[0]);
-        
-        $method = strtoupper($method);
-
-        $this->method = $method;
-    }
-
-    private function setRoute(): void
-    {
-        [$_, $route] = explode(" ", $this->rawSplited[0]);
-
-        $this->route = $route;
-    }
-
-    private function setHttpVersion(): void
-    {
-        [$_, $_, $httpVersion] = explode(" ", $this->rawSplited[0]);
-
-        $this->version = $httpVersion;
     }
 
     private function setFieldsAndValues(): void
@@ -64,10 +51,70 @@ class Headers {
         }
     }
     
-    public function set(string $key, string $value): void
-    {
-        $key = strtolower($key);
+    public function set(string $field, string $value): void
+    {   
+        $firstLetter = $field[0];
+        
+        $field = str_replace("-", "", $field);
+        $field[0] = strtolower($firstLetter);
 
-        $this->$key = $value;
+        $this->$field = $value;
+    }
+
+    public function toRaw(): void
+    {
+        $this->raw = implode(" ", [
+            $this->version,
+            $this->status,
+            $this->statusCodeToText($this->status)
+        ]);
+    }
+
+    private function statusCodeToText(string $code): string
+    {
+        $codes = [
+            "100"  => "Continue",
+            "101"  => "Switching Protocols",
+            "200"  => "OK",
+            "201"  => "Created",
+            "202"  => "Accepted",
+            "203"  => "Non-Authoritative Information",
+            "204"  => "No Content",
+            "205"  => "Reset Content",
+            "206"  => "Partial Content",
+            "300"  => "Multiple Choices",
+            "301"  => "Moved Permanently",
+            "302"  => "Found",
+            "303"  => "See Other",
+            "304"  => "Not Modified",
+            "305"  => "Use Proxy",
+            "307"  => "Temporary Redirect",
+            "400"  => "Bad Request",
+            "401"  => "Unauthorized",
+            "402"  => "Payment Required",
+            "403"  => "Forbidden",
+            "404"  => "Not Found",
+            "405"  => "Method Not Allowed",
+            "406"  => "Not Acceptable",
+            "407"  => "Proxy Authentication Required",
+            "408"  => "Request Time-out",
+            "409"  => "Conflict",
+            "410"  => "Gone",
+            "411"  => "Length Required",
+            "412"  => "Precondition Failed",
+            "413"  => "Request Entity Too Large",
+            "414"  => "Request-URI Too Large",
+            "415"  => "Unsupported Media Type",
+            "416"  => "Requested range not satisfiable",
+            "417"  => "Expectation Failed",
+            "500"  => "Internal Server Error",
+            "501"  => "Not Implemented",
+            "502"  => "Bad Gateway",
+            "503"  => "Service Unavailable",
+            "504"  => "Gateway Time-out",
+            "505"  => "HTTP Version not supported"
+        ];
+
+        return $codes[$code] ? $codes[$code] : $codes["404"];
     }
 }
--- tests/HeadersTest.php
+++ tests/HeadersTest.php
@@ -1,6 +1,5 @@
 <?php declare(strict_types=1);
 
-use PhpParser\Node\Expr\Cast\Object_;
 use PHPUnit\Framework\TestCase;
 use SparklePHP\Socket\Protocol\Http\Headers;
 
@@ -31,10 +30,10 @@ final class HeadersTest extends TestCase
             "route"           => "/favicon.ico",
             "version"         => "HTTP/1.1",
             "host"            => "localhost:8080",
-            "user-agent"      => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
+            "userAgent"      => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
             "accept"          => "image/avif,image/webp,*/*",
-            "accept-language" => "en-US,en;q=0.5",
-            "accept-encoding" => "gzip, deflate, br"
+            "acceptLanguage" => "en-US,en;q=0.5",
+            "acceptEncoding" => "gzip, deflate, br"
         ];
 
         $result = new Headers($raw);
