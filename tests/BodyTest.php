d480044ef761f31a3e631524b8cc046242d831d3
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-12-28 21:28:27 -0300

    test[Body]: More test to diferent data

--- tests/BodyTest.php
+++ tests/BodyTest.php
@@ -33,4 +33,56 @@ class BodyTest extends TestCase {
         
         $this->assertEquals($expected, (array)$actual);
     }
+
+    public function testTryParseApplicationJsonToRaw(): void
+    {
+        $expected = [
+            "raw"  => '{"key":"value"}',
+            "contentType" => "application/json",
+            "data" => [
+                "key" => "value"
+            ]
+        ];
+
+        $actual = new Body("");
+        $actual->set("contentType", "application/json");
+        $actual->set("data", [
+            "key" => "value"
+        ]);
+        $actual->toRaw();
+
+        $this->assertEquals($expected, (array)$actual);
+    }
+
+    public function testTryParseHTMLToRAW(): void
+    {
+
+        $data = <<<END
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>
+        END;
+
+        $expected = [
+            "raw" => $data,
+            "contentType" => "text/html",
+            "data" => $data
+        ];
+
+        $actual = new Body();
+        $actual->set("contentType", "text/html");
+        $actual->set("data", $data);
+        $actual->toRaw();
+
+        $this->assertEquals($expected, (array)$actual);
+    }
 }
