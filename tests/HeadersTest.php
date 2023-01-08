6cfb8c3b936675859a7a6cfe93653a68582a5d64
lipeRefosco <luisfelipe.refosco@hotmail.com>
2023-01-08 12:34:04 -0300

    test[Body]: separate arraySplited

--- tests/HeadersTest.php
+++ tests/HeadersTest.php
@@ -1,9 +1,9 @@
-<?php declare(strict_types=1);
+<?php
 
 use PHPUnit\Framework\TestCase;
 use SparklePHP\Socket\Protocol\Http\Headers;
 
-final class HeadersTest extends TestCase
+class HeadersTest extends TestCase
 {
     public function testHeaderClass():void
     {
@@ -16,22 +16,24 @@ final class HeadersTest extends TestCase
         Accept-Encoding: gzip, deflate, br
         END;
 
-        $expected = (Array)[
-            "raw"             => $raw,
-            "rawSplited"      => [
-                "GET /favicon.ico HTTP/1.1",
-                "Host: localhost:8080",
-                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
-                "Accept: image/avif,image/webp,*/*",
-                "Accept-Language: en-US,en;q=0.5",
-                "Accept-Encoding: gzip, deflate, br"
-            ],
-            "method"          => "GET",
-            "route"           => "/favicon.ico",
-            "version"         => "HTTP/1.1",
-            "host"            => "localhost:8080",
+        $rawSplited = [
+            "GET /favicon.ico HTTP/1.1",
+            "Host: localhost:8080",
+            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
+            "Accept: image/avif,image/webp,*/*",
+            "Accept-Language: en-US,en;q=0.5",
+            "Accept-Encoding: gzip, deflate, br"
+        ];
+
+        $expected = [
+            "raw"            => $raw,
+            "rawSplited"     => $rawSplited,
+            "method"         => "GET",
+            "route"          => "/favicon.ico",
+            "version"        => "HTTP/1.1",
+            "host"           => "localhost:8080",
             "userAgent"      => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
-            "accept"          => "image/avif,image/webp,*/*",
+            "accept"         => "image/avif,image/webp,*/*",
             "acceptLanguage" => "en-US,en;q=0.5",
             "acceptEncoding" => "gzip, deflate, br"
         ];
