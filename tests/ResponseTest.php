2601671465cf863e3d23b669f959843b0d48ac74
lipeRefosco <luisfelipe.refosco@hotmail.com>
2023-01-13 17:23:31 -0300

    test[Respose]: test to define what will be the default values of Headers

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 use SparklePHP\Socket\Protocol\Http\Body;
 use SparklePHP\Socket\Protocol\Http\Headers;
 use SparklePHP\Socket\Protocol\Http\Response;
+use SparklePHP\Socket\Protocol\Http\ResponseStatusException;
 
 class ResponseTest extends TestCase {
 
@@ -28,9 +29,24 @@ class ResponseTest extends TestCase {
         ];
         $expectedRes["headers"]->setStatus("200");
         $expectedRes["headers"]->setVersion("HTTP/1.1");
+        $expectedRes["headers"]->set("content-Type", "text/html; charset=UTF-8");
         $expectedRes["headers"]->toRaw();
         $expectedRes["body"]->set("data", ["AnyKey" => "any Value"]);
 
         $this->assertEquals($expectedRes, (array)$resposeTest);
+    }
+
+    public function testSetDefault(): void
+    {
+        $defaultRespose = new Response();
+        $defaultRespose->setDefault();
+
+        $responseHeadersExpectedFileds = [
+            "content-type" => "text/html; charset=UTF-8"
+        ];
+        $this->assertEquals(
+            $responseHeadersExpectedFileds,
+            $defaultRespose->headers->fields
+        );
     }
 }
