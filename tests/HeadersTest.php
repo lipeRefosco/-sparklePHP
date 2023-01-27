80bcf79bb431d7023a519624b54931f536325c9d
lipeRefosco <luisfelipe.refosco@hotmail.com>
2023-01-26 21:50:15 -0300

    test[Headers]: new property of class

--- tests/HeadersTest.php
+++ tests/HeadersTest.php
@@ -24,6 +24,7 @@ class HeadersTest extends TestCase
             "method"     => "GET",
             "route"      => "/favicon.ico",
             "version"    => "HTTP/1.1",
+            "query"      => [],
             "fields"     => [
                 "Host"            => "localhost:8080",
                 "User-Agent"      => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
@@ -35,14 +36,14 @@ class HeadersTest extends TestCase
 
         $result = new Headers($raw);
         $result->parseRaw();
-
+        
         $this->assertEquals($expected, (array)$result);
     }
 
     public function testHeaderParserMethodWithQueryParams(): void
     {
         $data = <<<END
-                GET /?teste=4 HTTP/1.1
+                GET /?bar=4&foo=24 HTTP/1.1
                 Host: localhost:8080
                 END;
 
@@ -57,7 +58,8 @@ class HeadersTest extends TestCase
             "method"     => "GET",
             "route"      => "/",
             "query"      => [
-                "teste" => 4
+                "bar" => "4",
+                "foo"   => "24"
             ],
             "version"    => "HTTP/1.1",
             "fields"     => [ "Host"  => "localhost:8080" ]
